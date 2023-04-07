40c845b5647350696dc2e9e8de0af6ce98ed4e6e
mohammadpanahnikzad <mohammadpanahnikzad@localhost>
2023-04-07 18:07:44 -0400

    Upcoming Projects .....

---
+++ src/project6/GitHubProject6.java
@@ -0,0 +1,13 @@
+package project6;
+
+public class GitHubProject6 {
+	
+	public static void main(String args []) {
+		
+		System.out.println("We will start new Project Soon...");
+		
+		
+		
+	}
+
+}
